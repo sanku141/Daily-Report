f3f1718bcd3075cee3bfb5c4b4969652729459d4
sanku141 <sanketnk1401@gmail.com>
2025-05-13 00:33:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -143,4 +143,8 @@ currentY += mergedHeight;
 doc.end()
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
